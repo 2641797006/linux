666f02618d3c35f1e6cea21d8fb01626f52230c8
2641797006 <2641797006@qq.com>
2018-12-23 19:51:08 +0800

    -

---
+++ c/include/pMacro.h
@@ -0,0 +1,43 @@
+#ifndef _PMACRO_H_
+#define _PMACRO_H_
+
+#define __lk_suffix(name, suffix)	name##_##suffix
+#define lk_suffix(name, suffix)		__lk_suffix(name, suffix)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
