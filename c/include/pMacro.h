f52c4fbae5215f548e445649224adac34299f8eb
2641797006 <2641797006@qq.com>
2018-12-24 16:20:12 +0800

    -

--- c/include/pMacro.h
+++ c/include/pMacro.h
@@ -4,41 +4,11 @@
 #define __lk_suffix(name, suffix)	name##_##suffix
 #define lk_suffix(name, suffix)		__lk_suffix(name, suffix)
 
+#define CopyElem(dest, src) memcpy(dest, src, sizeof(*(src)))
+#define CmpaElem_O(x, y) (x<y ? -1 : (x==y ? 0 : 1))
+#define CmpaElem(x, y) CmpaElem_O((x), (y))
+
 #define ln()				putchar('\n')
 #define PRINT_STR(str)			printf("%s", str)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #endif
--- c/include/pctype.h
+++ c/include/pctype.h
@@ -21,25 +21,4 @@ typedef unsigned long long QWORD;
 #define QWORD	unsigned long long
 #endif
 
-#define CopyElem(dest, src) memcpy(dest, src, sizeof(*(src)))
-#define CmpaElem_O(x, y) (x<y ? -1 : (x==y ? 0 : 1))
-#define CmpaElem(x, y) CmpaElem_O((x), (y))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #endif
