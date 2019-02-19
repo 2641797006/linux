d5cde3d1a5a2253f19895ce0a5b111547bc7f600
cpplay <2641797006@qq.com>
2019-02-19 22:44:29 +0800

    XDEngine

--- cpp/btree/3.0/bt_xde.h
+++ cpp/btree/3.0/bt_xde.h
@@ -1,7 +1,23 @@
+/*
+AddNode(XDE_Table *ptb);
+AddNode(XDE_Tree *ptr);
+DeleteNode(UINT uid);
+GetNode(UINT uid);
+
+XDE_Table, XDE_Tree, WorkGroup
+*/
+
 #include "btree.h"
+#include <random>
+
+class XDE_Table{};
+class XDE_Tree{};
+class WorkGroup{};
+
+enum{UT_XTable, UT_XTree, UT_WorkGroup};
 
 class XDE_BTC{
-  public:
+  public:	//先用public, 完成后再考虑改成private
 	unsigned int uid;
 	unsigned int utype;
 	void *p;
@@ -23,34 +39,46 @@ class XDE_BTC{
 
 class XDEngine{
   public:
-	XDE_BTC* find(unsigned int uid);
-	XDE_BTC* insert(unsigned int uid, void *p, unsigned int utype);
-	int erase(unsigned int uid);
+	XDE_BTC* GetNode(unsigned int uid);	//未找到时返回NULL, 成功则返回XDE_BTC类指针
+	unsigned int AddNode(XDE_Table *ptb) {return insert(ptb, UT_XTable);}//add成功返回uid
+	unsigned int AddNode(XDE_Tree  *ptr) {return insert(ptr, UT_XTree);} //失败返回0
+	unsigned int AddNode(WorkGroup *pwg) {return insert(pwg, UT_WorkGroup);}
+	int DeleteNode(unsigned int uid);	//删除成功返回1, 不存在则返回0
 
-	XDEngine(){tree.set_unique();}
+	XDEngine(){tree.set_unique(); rand.seed(666);} //uid是惟一的, 随机的
+
   private:
+	unsigned int insert(void *p, unsigned int utype);
+
 	XDE_BTC _xde;
+	std::default_random_engine rand;
 	__tree::btree<XDE_BTC> tree;
 };
 
 inline XDE_BTC*
-XDEngine::find(unsigned int uid)
+XDEngine::GetNode(unsigned int uid)
 {
 	_xde.uid = uid;
 	return tree.find(_xde);
 }
 
-inline XDE_BTC*
-XDEngine::insert(unsigned int uid, void *p=NULL, unsigned int utype=0)
+unsigned int
+XDEngine::insert(void *p, unsigned int utype)
 {
-	_xde.uid = uid;
+	int i;
+	_xde.uid = rand();
 	_xde.utype = utype;
 	_xde.p = p;
-	return tree.insert(_xde);
+	for (i=0; i<100; i++)
+		if (!tree.insert(_xde))
+			return _xde.uid;
+		else
+			_xde.uid = rand();
+	return 0;
 }
 
 inline int
-XDEngine::erase(unsigned int uid)
+XDEngine::DeleteNode(unsigned int uid)
 {
 	_xde.uid = uid;
 	return tree.erase(_xde);
