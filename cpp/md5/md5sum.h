793d337f3b8df4ee25e0157b4cbdd9d97c3b8fdd
cpplay <2641797006@qq.com>
2019-02-25 13:40:57 +0800

    md5

---
+++ cpp/md5/md5sum.h
@@ -0,0 +1,23 @@
+#ifndef _MD5SUM_H_
+#define _MD5SUM_H_
+
+namespace _24k{
+using namespace std;
+
+class md5sum
+{
+  public:
+	string const& md5(string const&);
+
+  private:
+	uint32_t A, B, C, D;
+	uint32_t a, b, c, d;
+	uint32_t x[16];
+	string s_null;
+	void init(){A=0x67452301, B=0xefcdab89, C=0x98badcfe, D=0x10325476;}
+	void loop();
+};
+
+} //namespace _24k
+
+#endif
