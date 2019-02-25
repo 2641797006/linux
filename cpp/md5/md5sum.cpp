7653ba974391c50f7584eb832f40ebcf897110db
cpplay <2641797006@qq.com>
2019-02-25 15:57:38 +0800

    md5

--- cpp/md5/md5sum.cpp
+++ cpp/md5/md5sum.cpp
@@ -20,30 +20,12 @@
 #define HH(a, b, c, d, x, str, ac) a = b + (RL((a + H(b,c,d) + x + ac),str))
 #define II(a, b, c, d, x, str, ac) a = b + (RL((a + I(b,c,d) + x + ac),str))
 
-#define _24k_MD5_UPPER	0x0001
+#ifndef _MD5SUM_H_
+#include "md5sum.h"
+#endif
 
 namespace _24k{
 using namespace std;
-
-class md5sum
-{
-  public:
-	void set_upper(){flag|=_24k_MD5_UPPER;}
-	void set_lower(){flag&=~_24k_MD5_UPPER;}
-	string const& operator()(string const&);
-	string const& operator()(string const& str, string const& salt){return (*this)(str+salt);}
-
-	md5sum():flag(0){}
-
-  private:
-	uint32_t flag;
-	uint32_t A, B, C, D;
-	uint32_t a, b, c, d;
-	uint32_t x[16];
-	string s_null;
-	void init(){A=0x67452301, B=0xefcdab89, C=0x98badcfe, D=0x10325476;}
-	void loop();
-};
 
 string const&
 md5sum::operator()(string const& str)
