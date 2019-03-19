bf9adbf894497d68231291d7f6213453a1c4fdd9
cpplay <2641797006@qq.com>
2019-03-19 19:54:32 +0800

    -

--- java/ex2/BigInt.java
+++ java/ex2/BigInt.java
@@ -25,9 +25,15 @@ public:
 	BigInt mul(long);
 	BigInt mul(String);
 	BigInt mul(BigInt);
-	// div
 
+	BigInt div(long);
+	BigInt div(String);
+	BigInt div(BigInt);
+
+	int cmp(long);
+	int cmp(String);
 	int cmp(BigInt);
+
 	long toLong();
 	String toString();
 */
@@ -117,7 +123,7 @@ public:
 	}
 
 	private int u_sub(BigInt big) { // unsign sub
-		if (abscmp(big) < 0) {
+		if (u_cmp(big) < 0) {
 			var big_t = new BigInt(big);
 			big_t._bs_sub(this);
 			u_assign(big_t);
@@ -191,6 +197,74 @@ public:
 		return this;
 	}
 
+	public BigInt div(long li) {
+		return div(new BigInt(li));
+	}
+
+	public BigInt div(String s) {
+		return div(new BigInt(s));
+	}
+
+	public BigInt div(BigInt big) {
+		if (big.cmp(0) == 0) // x/0 should return null
+			return null;
+		sign = sign==big.sign ? true : false;
+		u_div(big);
+		rm_zero();
+		zerofix();
+		return this;
+	}
+
+	private BigInt u_div(BigInt big) { // unsign div
+		int i;
+		BigInt b1, bq;
+
+		b1 = new BigInt();
+		bq = new BigInt();
+		i = num.size() - big.num.size();
+		bq._en_num(i+1);
+		for (; i>=0; --i) {
+			b1.num.clear();
+			b1._cp_num(i, this);
+			bq.num.set(i, (byte)b1._el_div(big));
+			_cp_num(b1, i);
+			i -= rm_zero();
+		}
+		u_assign(bq);
+		return this;
+	}
+
+	private int _el_div(BigInt big) { // unsign && same size
+		int i=0;
+		while (u_cmp(big) >= 0) {
+			_bs_sub(big);
+			++i;
+		}
+		return i;
+	}
+
+	private void _cp_num(int index, BigInt big) { // copy big.num[index] to num
+		int i, n=big.num.size()-index;
+		for (i=0; i<n; ++i)
+			num.add(big.num.get(i+index));
+	}
+
+	private void _cp_num(BigInt big, int index) { // copy big.num to num[index]
+		int i, n=big.num.size()+index;
+		for (i=index; i<n; ++i)
+			num.set(i, big.num.get(i-index));
+		n = num.size();
+		while (--n>=i)
+			num.remove(n);
+	}
+
+	private void _en_num(int n) {
+		int i = num.size();
+		num.ensureCapacity(n);
+		for (; i<n; ++i)
+			num.add((byte)0);
+	}
+
 	public void set(long n) {
 		int i;
 		byte b;
@@ -245,13 +319,21 @@ public:
 		return li;
 	}
 
+	public int cmp(long li) {
+		return cmp(new BigInt(li));
+	}
+
+	public int cmp(String s) {
+		return cmp(new BigInt(s));
+	}
+
 	public int cmp(BigInt big) {
 		if (sign != big.sign)
 			return sign ? 1 : -1;
-		return sign ? abscmp(big) : -abscmp(big);
+		return sign ? u_cmp(big) : -u_cmp(big);
 	}
 
-	private int abscmp(BigInt big) { // num compare, ignore sign
+	private int u_cmp(BigInt big) { // num compare, ignore sign
 		int diff = num.size() - big.num.size();
 		if (diff != 0)
 			return diff;
@@ -279,13 +361,14 @@ public:
 		num.set(index, (byte)(num.get(index)-1));
 	}
 
-	private void rm_zero() { // remove the high zero
+	private int rm_zero() { // remove the high zero
 		int i = num.size();
 		while (--i>=1)
 			if (num.get(i) == 0)
 				num.remove(i);
 			else
 				break;
+		return num.size() - i - 1;
 	}
 
 	private void zerofix() { // 0 must be +0
