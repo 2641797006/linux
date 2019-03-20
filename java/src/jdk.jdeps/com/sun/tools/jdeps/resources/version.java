901ede5bbf128470354491e3b83b3db5f4ed1309
cpplay <2641797006@qq.com>
2019-03-20 08:24:19 +0800

    openjdk-11-source

---
+++ java/src/jdk.jdeps/com/sun/tools/jdeps/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.jdeps.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "10.0.2+13-Ubuntu-1ubuntu0.18.04.4" },
+            { "jdk", "10.0.2" },
+            { "release", "10.0.2" },
+        };
+    }
+}
