9b82f9eb50318dac3905cabc800fc47deb8f68b2
cpplay <2641797006@qq.com>
2019-03-29 07:50:57 +0800

    -

--- c/template/vector/2.0/vector.h
+++ c/template/vector/2.0/vector.h
@@ -1,17 +1,28 @@
 #ifndef _24k_vector_t
-#error: No define "_24k_vector_t"
-#endif
+# error: No define "_24k_vector_t"
+#else
 
 /*
  * T = _24k_vector_t
  * T_vector // vector type
+ * T_vector_iterator // itetator type
+ *
  * bool T_vector_init (T_vector *vec) // init T_vector vec
  * void T_vector_destroy (T_vector *vec) // destroy T_vector vec
+ *
  * bool T_vector_assign (T_vector *vec, size_t count, T *t) // 清空vec 并且 插入count个t
  * T* T_vector_at (T_vector *vec, size_t pos) // 获取vec中第pos个元素的地址
- * T* T_vector_front (T_vector *vec) // 获取vec首个元素的地址
- * T* T_vector_back (T_vector *vec) // 获取vec最后一个元素的地址
+ * T T_vector_front (T_vector *vec) // 获取vec首个元素
+ * T T_vector_back (T_vector *vec) // 获取vec最后一个元素
  * T* T_vector_data (T_vector *vec) // 返回vec的T数组首地址
+ *
+ * T_vector_iterator T_vector_first (T_vector *vec) // 返回指向首个元素的迭代器
+ * T_vector_iterator T_vector_last (T_vector *vec) // 返回指向最后一个元素的迭代器
+ * T_vector_iterator T_vector_head (T_vector *vec) // 首个元素前一个位置
+ * T_vector_iterator T_vector_tail (T_vector *vec) // 最后一个元素后一个位置
+ * bool T_vector_prev (T_vector_iterator *pit) // pit:迭代器地址, 使迭代器指向前一个元素
+ * bool T_vector_next (T_vector_iterator *pit) // pit:迭代器地址, 使迭代器指向后一个元素
+ *
  * bool T_vector_empty (T_vector *vec) // 判断vec是否为空
  * size_t T_vector_size (T_vector *vec) // 返回vec元素个数
  * bool T_vector_reserve (T_vector *vec, size_t new_cap) // 保证vec可以容纳new_cap个元素, 避免多次扩展内存
@@ -19,11 +30,11 @@
  * void T_vector_shrink_to_fit (T_vector *vec) // 释放多余的内存
  * void T_vector_clear (T_vector *vec) // 清空vec中所有元素
  * bool T_vector_resize (T_vector *vec, size_t count) // 调整vec元素个数, 新增元素将被初始化为0
+ *
  * bool T_vector_insert (T_vector *vec, size_t pos, T *t) // 在vec的pos位置插入元素t
  * bool T_vector_erase (T_vector *vec, size_t pos) // 擦除vec中pos位置的元素
  * bool T_vector_push_back (T_vector *vec, T *t) // 在vec末尾插入元素t
  * bool T_vector_pop_back (T_vector *vec) // 删除vec最后一个元素
- * int T_vector_traverse (T_vector *vec, int (*visit)(T*)) // 使用visit函数顺序访问vec中所有元素, 当visit返回非0时终止并返回该值
  *
  */
 
@@ -310,3 +321,5 @@ _24k(assign) (_24k_vector *vec, size_t count, _24k_vector_t *t)
 #undef _24k_expand_times
 
 #undef _24k_vector_t
+
+#endif
