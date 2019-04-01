29e1f6d56b989500bacf1a02775404927663897c
cpplay <2641797006@qq.com>
2019-04-01 09:41:13 +0800

    -

--- java/ex4/Gomoku.java
+++ java/ex4/Gomoku.java
@@ -1,25 +1,128 @@
 import java.lang.Math;
+import javafx.scene.paint.Paint;
+import javafx.scene.paint.Color;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.canvas.GraphicsContext;
 
 public class Gomoku extends GomokuDraw {
+	// 当前棋子颜色
 	boolean flag;
+	// 有效点击范围
 	double click_range;
+	// 星位色彩宽高
+	Paint star_stroke, star_fill;
+	double star_width, star_height;
+	byte[][] CM;
+	//
+	int dragon;
 
 	public Gomoku()
 	{
 		flag = false;
+		dragon = 5;
+		CM = new byte[this.chessboard_row+2][this.chessboard_col+2];
 		setClickRange(this.grid_width * 3.9/10.0);
+		setStarColor(null, Color.web("#888A85"));
+		setStarSize(this.grid_width/4.0, this.grid_height/4.0);
+		setStar();
 		canvas.setOnMouseClicked(new Message(this));
 	}
+
+	Gomoku setStarColor( Paint stroke, Paint fill )
+	{
+		this.star_stroke = stroke;
+		this.star_fill = fill;
+		return this;
+	}
+
+	Gomoku setStarSize( double width, double height )
+	{
+		this.star_width = width;
+		this.star_height = height;
+		return this;
+	}
+
+	public Gomoku setStar()
+	{
+		setStar( 3, 3);
+		setStar( 9, 3);
+		setStar(15, 3);
+		setStar( 3, 9);
+		setStar( 9, 9);
+		setStar(15, 9);
+		setStar( 3, 15);
+		setStar( 9, 15);
+		setStar(15, 15);
+		return this;
+	}
+
+	Gomoku setStar(int row, int col)
+	{
+		return setStar( getGC(), row, col );
+	}
+
+	Gomoku setStar(GraphicsContext gc, int row, int col)
+	{
+		setChessman( gc, row, col, this.star_stroke, this.star_fill, this.star_width, this.star_height,
+				this.chessboard_startX, this.chessboard_startY,
+				this.grid_width, this.grid_height );
+		return this;
+	}
+
+	boolean win(int row, int col)
+	{
+		if (stat(row, col, 1, 0) >= dragon)
+			return true;
+		if (stat(row, col, 0, 1) >= dragon)
+			return true;
+		if (stat(row, col, 1, 1) >= dragon)
+			return true;
+		if (stat(row, col, 1, -1) >= dragon)
+			return true;
+		return false;
+	}
+
+	int stat(int row, int col, int ra, int ca)
+	{
+		++row;
+		++col;
+		int r, c, n=1;
+		byte key = CM[row][col];
+		r = row + ra;
+		c = col + ca;
+		while ( CM[r][c] == key ) {
+			r += ra;
+			c += ca;
+			++n;
+		}
+		r = row - ra;
+		c = col - ca;
+		while ( CM[r][c] == key ) {
+			r -= ra;
+			c -= ca;
+			++n;
+		}
+		return n;
+	}
+
+/*----- handle -----*/
 
 	public void handle(MouseEvent event)
 	{
 		int c[]=click(event.getX(), event.getY());
-		if ( c == null )
+		if ( c == null || CM [c[0]+1] [c[1]+1] != 0 )
 			return;
 		setChessman(c[0], c[1], flag);
-		flag = flag ? false : true;
+		if ( flag ) {
+			CM [c[0]+1] [c[1]+1] = 1;
+			flag = false;
+		} else {
+			CM [c[0]+1] [c[1]+1] = -1;
+			flag = true;
+		}
+		if ( win(c[0], c[1]) )
+			System.out.println((flag ? "black" : "white")+" win");
 	}
 
 	public Gomoku setClickRange(double d)
