e13df5d950d246ab96adc8247dee32d01d66d231
cpplay <2641797006@qq.com>
2019-04-04 17:07:15 +0800

    -

---
+++ mariaDB/ex/ex4.sql
@@ -0,0 +1,35 @@
+--1
+drop table if exists `cc`;
+create table `cc` (
+	`Cc1` varchar(20) default null,
+	`Cc2` int default null,
+	`Cc3` dec(10,2) default null,
+	`Cc4` varchar(60) default null
+) engine=InnoDB default charset=utf8mb4;
+
+--2
+lock tables `cc` write;
+insert into `cc` values
+	('赵一', 20, 580.00, '重邮宿舍12-3-5'),
+	('钱二', 19, 540.00, '南福苑5-2-9'),
+	('孙三', 21, 555.50, '学生新区21-5-15'),
+	('李四', 22, 480.00, '重邮宿舍8-2-22'),
+	('周五', 20, 495.50, '学生新区23-4-8'),
+	('吴六', 19, 435.00, '南福苑2-5-12');
+unlock tables;
+
+--2(print)
+select * from `cc`;
+
+--3
+update `cc` set `Cc3`=`Cc3`+5 where `Cc2`<=20;
+
+--3(print)
+select * from `cc`;
+
+--4
+delete from `cc` where `Cc2`>=20 and `Cc3`>=500;
+
+--4(print)
+select * from `cc`;
+
