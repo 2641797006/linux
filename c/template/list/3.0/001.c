23255bbb4b06949a149d8c4778fe118e53bc0bf4
cpplay <2641797006@qq.com>
2019-05-06 22:37:30 +0800

    -

--- c/template/list/3.0/001.c
+++ c/template/list/3.0/001.c
@@ -16,37 +16,55 @@ main()
 {
 	int i;
 	stu s = { 'A', 0 };
-	int_list v1;
-	stu_list v2;
+	int_list L1;
+	stu_list L2;
 
-	int_list_init(&v1);
-	stu_list_init(&v2);
+	int_list_init(&L1);
+	stu_list_init(&L2);
 
 	for ( i=0; i<30; ++i ) {
-		int_list_push_back(&v1, &i);
+		int_list_push_back(&L1, &i);
 		s.i = i+60000;
-		stu_list_push_back(&v2, &s);
+		stu_list_push_back(&L2, &s);
 	}
 
 	for ( i=0; i<10; ++i ) {
-		int_list_pop_back(&v1);
-		stu_list_pop_back(&v2);
+		int_list_pop_back(&L1);
+		stu_list_pop_back(&L2);
 	}
 
-	int_list_iterator it1 = int_list_first(&v1);
-	stu_list_iterator it2 = stu_list_first(&v2);
+	int_list_iterator it1 = int_list_first(&L1);
+	stu_list_iterator it2 = stu_list_first(&L2);
 
-	while ( it1 != int_list_tail(&v1) ) {
+// 遍历L1:
+	while ( it1 != int_list_tail(&L1) ) {
 		printf("%d ", *it1);
 		it1 = int_list_next(it1);
 	}
 	puts("");
-	while ( it2 != stu_list_tail(&v2) ) {
+
+// 遍历L2:
+	while ( it2 != stu_list_tail(&L2) ) {
 		printf("[%c,%d] ", it2->c, it2->i);
 		it2 = stu_list_next(it2);
 	}
 	puts("");
-	int_list_destroy(&v1);
-	stu_list_destroy(&v2);
+
+	int_list_save(&L1, "0"); // 保存L1到文件0
+	int_list L3;
+	int_list_load(&L3, "0"); // 从文件0加载list到L3
+
+// 逆序遍历L3:
+	int_list_iterator it3 = int_list_last(&L3);
+	while ( it3 != int_list_head(&L3) ) {
+		printf("%d ", *it3);
+		it3 = int_list_prev(it3);
+	}
+	puts("");
+
+	int_list_destroy(&L3);
+
+	int_list_destroy(&L1);
+	stu_list_destroy(&L2);
 }
 
