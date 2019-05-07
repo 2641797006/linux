d817ef49d03591363d401661ee8a19d7904aba23
cpplay <2641797006@qq.com>
2019-05-07 12:04:52 +0800

    -

--- java/experiment/mt1/xx1.java
+++
@@ -1,9 +0,0 @@
-public class xx1{
-	public static void main(String[] args) throws Exception {
-
-	}
-
-	static void print(String s) {System.out.print(s);}
-	static void println(String s) {System.out.println(s);}
-	static void printf(String fmt, Object... args) {System.out.printf(fmt, args);}
-}
