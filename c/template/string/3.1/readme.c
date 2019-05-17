02a0a381a810cc2fe9e00f8ab9ea4e320e4bdeb5
cpplay <2641797006@qq.com>
2019-05-18 07:33:00 +0800

    -

--- c/template/string/3.1/readme.c
+++ c/template/string/3.1/readme.c
@@ -38,11 +38,11 @@ string* clear(string *s);
 string* resize(string *s, size_t count);
 	// 强行调整s的长度
 
-string* insert(string *s, string_iterator it, char c);
-	// 在it位置插入字符c
+string* insert(string *s, size_t pos, char c);
+	// 在pos位置插入字符c
 
-string* erase(string *s, string_iterator it);
-	// 删除it位置的字符
+string* erase(string *s, size_t pos);
+	// 删除pos位置的字符
 
 string* push_back(string *s, char c);
 	// 在s的末尾追加字符c
@@ -68,4 +68,35 @@ string* replace(string *s, size_t pos, size_t count, const char *cs);
 int find(string *s, size_t pos, const char *cs);
 	// 从s的pos位置开始查找cs字符串
 
+int find_first_of(const string *s, size_t pos, const char *cs);
+	// 从s的pos位置开始查找 包含在 cs字符串 中的字符, 返回其下标, 没找到返回-1
 
+int find_first_not_of(const string *s, size_t pos, const char *cs);
+	// 从s的pos位置开始查找 不在 cs字符串 中的字符, 返回其下标, 没找到返回-1
+
+int find_last_of(const string *s, size_t pos, const char *cs);
+	// 从s的pos位置开始[向前]查找 包含在 cs字符串 中的字符, 返回其下标, 没找到返回-1
+
+int find_last_not_of(const string *s, size_t pos, const char *cs);
+	// 从s的pos位置开始[向前]查找 不在 cs字符串 中的字符, 返回其下标, 没找到返回-1
+
+string* reverse(string *s);
+	// 将字符串内容倒置
+
+string* fgetline(string *s, FILE *fp, int delim);
+	// 从文件fp读取字符到s, 自动扩展s内存, 直到遇到字符delim, 读取并抛弃字符delim
+
+string* getline(string *s);
+	// 读取一行, 相当于 fgetline(s, stdin, '\n');
+
+string* fgets(string *s, FILE *fp);
+	// 从文件读取字符串 相当于 fscanf fp,%s  忽略前后空白字符
+
+string* gets(string *s);
+	// 相当于 fgets(s, stdin);
+
+string* string_init(string *s);
+	// 初始化string s
+
+void destroy(string *s);
+	// 释放s申请的内存, s将被置于 未初始化 状态
