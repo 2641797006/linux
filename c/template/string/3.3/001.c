925151b9b86298e7b64dd19cc2c2e3590d609a45
cpplay <2641797006@qq.com>
2019-06-02 16:19:47 +0800

    -

---
+++ c/template/string/3.3/001.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "string24k.h"
+
+int
+main()
+{
+	string _s, *s=&_s;
+
+	string_init(s)->assign(s, "Please input a string: ");	// 初始化s, 并赋值
+//	string_init(s); s->assign(s, "Please input a string: ");
+
+	puts( s->c_str(s) );	// 输出字符串s
+
+	s->gets(s);	// 输入一个字符串 ((相当于 scanf %s
+
+	puts( s->c_str(s) );	// 输出字符串s
+
+	s->getline(s);	// 清空缓冲区, s->gets(s)会残留换行符
+	s->getline(s);	// 读取一行
+
+	puts( s->c_str(s) );	// 输出字符串s
+
+	s->append(s, "end");	// 在 s 末尾追加字符串
+
+	puts( s->c_str(s) );	// 输出字符串s
+
+	printf("s->length = %ld\n", s->length(s)); // s长度
+
+	string _s1, *s1=&_s1;
+
+	string_init(s1)->assign(s1, "This is s1");
+	s1->swap(s1, s);
+
+	printf("swap:\ns = %s\ns1 = %s\n", s->c_str(s), s1->c_str(s1));
+
+	return 24-'k';
+}
