8d7e86dbdc7506e1d56ac0f8c24b95a54e80b11d
cpplay <2641797006@qq.com>
2019-10-12 14:51:57 +0800

    -

--- cpp/matrix/001.cpp
+++ cpp/matrix/001.cpp
@@ -1,30 +1,21 @@
 #include <iostream>
-#include <set>
 #include "matrix.h"
 using namespace std;
 using namespace akm;
 
 int main()
 {
-	srand(time(NULL));
-
-	Matrix<int> m1({
-		{4, -1, 2, 1},
-		{1, 1, 0, 3},
-		{0, 3, 1, 4},
-	}),
-	m2({
-		{1, 2},
-		{0, 1},
-		{3, 0},
-		{-1, 2}
-	}),
-	m3({
+	Matrix<double> m1({
 		{1, 2, 3},
 		{2, 2, 1},
 		{3, 4, 3}
-	});
-	cout<<m3<<endl;
-	cout<<m3.value()<<endl;
+	}), m2(3,3);
+	m2.E();
+
+	cout<<m1<<m2<<endl;
+
+	m2 /= m1;
+
+	cout<<m1<<m2<<endl;
 }
 
