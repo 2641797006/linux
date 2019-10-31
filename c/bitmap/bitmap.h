ca8f7494588fe2704df943a22d0b761ba4780c85
cpplay <2641797006@qq.com>
2019-10-31 12:42:08 +0800

    -

--- c/bitmap/bitmap.h
+++ c/bitmap/bitmap.h
@@ -6,6 +6,10 @@
 #include <stdbool.h>
 #include <memory.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef uint64_t QWORD;
 
 typedef struct BitMap {
@@ -14,145 +18,19 @@ typedef struct BitMap {
 	QWORD size; // bit num;
 }BitMap;
 
-BitMap*
-bitmap_init(BitMap *this, QWORD n) {
-	this->qsize = n/64 + ((n%64) ? 1 : 0);
-	this->data = (QWORD*)malloc(this->qsize * 8);
-	if ( ! this->data )
-		return NULL;
-	this->size = n;
-	return this;
-}
+BitMap* bitmap_init (BitMap*, QWORD);
+void bitmap_destroy (BitMap*);
+void bitmap_clear (BitMap*);
 
-void
-bitmap_clear(BitMap *this)
-{
-	memset(this->data, 0, this->qsize*8);
-}
+BitMap* create_bitmap (QWORD);
+bool bitmap_is_set (BitMap*, QWORD);
+void bitmap_set (BitMap*, QWORD);
+void bitmap_reset (BitMap*, QWORD);
+QWORD bitmap_find_first (BitMap*, bool*);
 
-void
-bitmap_destroy(BitMap *this)
-{
-	free(this->data);
-	this->size = 0;
-}
-
-BitMap*
-create_bitmap(QWORD n)
-{
-	BitMap *bmap = (BitMap*)malloc(sizeof(BitMap));
-	if ( ! bmap || ! bitmap_init(bmap, n) )
-		return NULL;
-	bitmap_clear(bmap);
-	return bmap;
-}
-
-bool
-bitmap_is_set(BitMap *this, QWORD pos)
-{
-	--pos;
-	QWORD x = pos/64;
-	QWORD y = pos%64;
-	QWORD q = ((QWORD)1) << y;
-	return (this->data[x] & q) ? true : false;
-}
-
-void
-bitmap_set(BitMap *this, QWORD pos)
-{
-	--pos;
-	QWORD x = pos/64;
-	QWORD y = pos%64;
-	QWORD q = ((QWORD)1) << y;
-	this->data[x] |= q;
-}
-
-void
-bitmap_reset(BitMap *this, QWORD pos)
-{
-	--pos;
-	QWORD x = pos/64;
-	QWORD y = pos%64;
-	QWORD q = ((QWORD)1) << y;
-	this->data[x] &= ~q;
-}
-
-QWORD
-qword_lowest_bit(QWORD q) // 0 ~ 63
-{
-	QWORD n = 63;
-	if (q & 0x00000000ffffffff) {
-		n -= 32;
-		q &= 0x00000000ffffffff;
-	}
-	if (q & 0x0000ffff0000ffff) {
-		n -= 16;
-		q &= 0x0000ffff0000ffff;
-	}
-	if (q & 0x00ff00ff00ff00ff) {
-		n -= 8;
-		q &= 0x00ff00ff00ff00ff;
-	}
-	if (q & 0x0f0f0f0f0f0f0f0f) {
-		n -= 4;
-		q &= 0x0f0f0f0f0f0f0f0f;
-	}
-	if (q & 0x3333333333333333) {
-		n -= 2;
-		q &= 0x3333333333333333;
-	}
-	if (q & 0x5555555555555555) {
-		n -= 1;
-	}
-	return n;
-}
-
-QWORD
-qword_highest_bit(QWORD q) // 0 ~ 63
-{
-	QWORD n = 0;
-	if (q & 0xffffffff00000000) {
-		n += 32;
-		q &= 0xffffffff00000000;
-	}
-	if (q & 0xffff0000ffff0000) {
-		n += 16;
-		q &= 0xffff0000ffff0000;
-	}
-	if (q & 0xff00ff00ff00ff00) {
-		n += 8;
-		q &= 0xff00ff00ff00ff00;
-	}
-	if (q & 0xf0f0f0f0f0f0f0f0) {
-		n += 4;
-		q &= 0xf0f0f0f0f0f0f0f0;
-	}
-	if (q & 0xcccccccccccccccc) {
-		n += 2;
-		q &= 0xcccccccccccccccc;
-	}
-	if (q & 0xaaaaaaaaaaaaaaaa) {
-		n += 1;
-	}
-	return n;
-}
-
-QWORD
-bitmap_find_first(BitMap *this, bool *is_ok)
-{
-	QWORD i;
-	for (i=0; i<this->qsize; ++i)
-		if (this->data[i])
-			break;
-	if (i == this->qsize) {
-		if (is_ok)
-			*is_ok = false;
-		return i;
-	}
-	if (is_ok)
-		*is_ok = true;
-	return i*64 + qword_lowest_bit(this->data[i]) + 1;
-}
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif // _BITMAP_H_
 
